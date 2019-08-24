66f5931313e0c4d19db5c945a7b22a2576e4bb0a
Andrew Obiano <andrew.obiano@gmail.com>
2019-08-24 09:24:31 -1000

    Finished

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
+"use strict";
+
 function scoreCalculator(arr) {
   // do work here
+  let finalScore = 0;
+
+  if (arr.length === 0) {
+    finalScore = -1;
+  }
+
+  // Scoring functions
+  arr.forEach(element => {
+    if (element > 0 && element < 5) {
+      finalScore += 10;
+    } else if (element >= 5 && element <= 10) {
+      finalScore += 5;
+    }
+  });
+
+  // Bonus scoring function
+  if (
+    arr.every(element => {
+      return element < 5;
+    }) &&
+    arr.length > 0
+  ) {
+    finalScore += 100;
+  }
+
+  console.log(arr);
+  console.log(finalScore);
+  return finalScore;
 }
 
 module.exports = scoreCalculator;
+
+let testArr1 = [1, 5, 11];
+let testArr2 = [15, 20, 30];
+let testArr3 = [];
+let testArr4 = [1, 2, 1, 4, 4, 2];
+scoreCalculator(testArr4);
--- test/spec.js
+++ test/spec.js
@@ -1,1 +1,34 @@
 // write tests here
+const chai = require("chai");
+const expect = chai.expect;
+const scoreCalculator = require("../index.js");
+
+describe("scoreCalculator", () => {
+  it("should be a function", () => {
+    expect(scoreCalculator).to.be.a("function");
+  });
+
+  it("should return a number", () => {
+    let testArray = [1, 5, 15];
+    let finalScore = scoreCalculator(testArray);
+    expect(finalScore).to.be.a("number");
+  });
+
+  it("should calculate dart scores correctly", () => {
+    let array1 = [1, 5, 15];
+    let array2 = [15, 20, 30];
+    let array3 = [];
+    let finalScore1 = scoreCalculator(array1);
+    let finalScore2 = scoreCalculator(array2);
+    let finalScore3 = scoreCalculator(array3);
+    expect(finalScore1).to.equal(15);
+    expect(finalScore2).to.equal(0);
+    expect(finalScore3).to.equal(-1);
+  });
+
+  it("should give a bonus 100 points appropriately", () => {
+    let array = [1, 2, 1, 4, 4, 2];
+    let finalScore = scoreCalculator(array);
+    expect(finalScore).to.equal(160);
+  });
+});
